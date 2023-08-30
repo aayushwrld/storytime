164b59d13e989b1bc296ebc2caa8ace425ca2437
aayushwrld <aayush.arora@kalvium.community>
2023-08-30 17:52:54 +0530

    commit2

--- app.js
+++ app.js
@@ -8,11 +8,26 @@ function moreAboutHome(address,distanceFromTown,hasNeighbours){
 //Trial 2:
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
+function moreAboutKaren(parents, noOfSiblings, isNuclearFamliy){
+    if(typeof(parents)=="string"&& typeof(noOfSiblings)=="number"&& typeof(isNuclearFamliy)=="boolean"){
+        return true
+    }else{
+        return false
+    }
+}
 
 //Trial 3:
 //Lily is suspicious about Karen's new friend
 //Karen tells her friend's age and even writes it down
 //Check which one those is not a number (NaN) and return that value
+
+function doesFriendExist(ageInText, ageInNumber){
+    if(isNaN(ageInText)){
+        return ageInText
+    }else if (isNaN(ageInNumber)){
+        return ageInNumber
+    }
+}
 
 //Trial 4:
 //Lily gave Karen x sweets
@@ -21,17 +36,45 @@ function moreAboutHome(address,distanceFromTown,hasNeighbours){
 //Her friend divided the remaining sweets into 2 parts for each
 //How many sweets did her friend get to eat?
 
+function sweetTooth(totalNoOfSweets, sweetsConsumedByKaren, sweetsConsumedInNMeters, metersToTravel){
+    var remaining = totalNoOfSweets - (sweetsConsumedByKaren + sweetsConsumedInNMeters*metersToTravel)
+
+    return  remaining/2
+}
+
 //Trial 5:
 //As Lily moves closer, it gets colder. She checks the temperature on her mobile
 //It only shows in fahrenheit. Convert the data to celsius and return it.
 
-//Trial 6:
+function convertToCelsius(fahrenheit){
+    var temperature = (((fahrenheit - 32) * 5)/9)
+    return temperature
+}
+
+//Trial 6:  
 //Lily can now do multiple things to deal with this
 //1. Take her daughter to a doctor
 //2. Talk to her husband about it
 //3. Counsel her daughter herself
 //4. Lock her daughter in her room
 //Given a value, return which of these above actions Lily would take
+
+function aDifficultChoice(choice){
+    switch(choice) {
+        case 1:
+            return "Take her daughter to a doctor"
+        break;
+        case 2:
+            return "Talk to her husband about it"
+        break;
+        case 3:
+            return "Counsel her daughter herself"
+        break
+        case 3:
+            return "Lock her daughter in her room"
+        break
+    }
+}
 
 //Challenge 1:
 //Lily realized that she'd hurt her daughter
@@ -41,3 +84,7 @@ function moreAboutHome(address,distanceFromTown,hasNeighbours){
 //Take all of Lily's strategies and concatenate them to a single var
 //Seperate the strategies by a single space
 //Return the length of the complete strategy
+
+function consoleKaren(strategies){
+    return strategies.join(" ").length
+}
